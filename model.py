9af28a50dbebc47ce9be026b4118ad0a6469dece
Manav Mehra <manavmehra96@gmail.com>
2018-03-26 15:51:12 +0530

    final model

--- model.py
+++ model.py
@@ -7,12 +7,13 @@ import requests
 import json
 import urllib.request
 import numpy as np
+import pickle
 
 
-firebase_url = 'https://hydroponics-2018.firebaseio.com/'
+#firebase_url = 'https://hydroponics-2018.firebaseio.com/'
 data = pd.read_csv('/home/manav/Downloads/data.csv')
 
-data = data[20:]
+
 #print (data.head())
 
 #filter_data = data.dropna(axis=0)
@@ -41,23 +42,26 @@ model.fit(X,y)
 
 train_X, val_X, train_y, val_y = train_test_split(X,y,random_state=0)
 
-print("Predicted Value = ")
-val_X=[[421,35,0]]
+
 
 #print(val_X)
 #print(val_y)
 
 
-
+#val_X=[[125,66,32]]
 
 train_model = model.fit(train_X,train_y)
 
 
 value = model.predict(val_X)
-print(value)
+print("Predicted Value = ", value)
 #print(mean_absolute_error(val_y,model.predict(val_X)))
-
+#score = model.score(X,y)
+#print(score)
 #train_X, val_X, train_y, val_y = train_test_split(X,y,random_state=0)
+
+filename = 'finalized_model.sav'
+pickle.dump(model, open(filename, 'wb'))
 
 
 
@@ -66,36 +70,9 @@ print(value)
 #print(value)
 #print(mean_absolute_error(val_y,value))
 
+#val_X=[[278,43,1]]
 
-
-
-
-
-
-
-
-
-
-
-
-
-a = value.size
-i=0
-
-#while a>0:
-   # data = {'value': value[i]}
-    #requests.post(firebase_url + '/' + '/value.json', data=json.dumps(data))
-    #print(value[i])
-   # a-=1
-   # i+=1
-
-#with urllib.request.urlopen("https://hydroponics-2018.firebaseio.com/.json?print=pretty&format=export&download=hydroponics-2018-export.json&auth=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MjE2MTc0MTksImV4cCI6MTUyMTYyMTAxOSwidiI6MCwiYWRtaW4iOnRydWV9.UBxEYISB9ebjsVv3_p3PrepoBa0gZ0P51YfVLyF3DRQ") as url:
- #   data = json.loads(url.read().decode())
-
-
-
-#for key in data["value"]:
- #   print(data["value"][key]["value"])
-    	
-   
- 
+#loaded_model = pickle.load(open('/home/manav/Desktop/Manav/Projects/Hydroponics/finalized_model.sav', 'rb'))
+#result = loaded_model.score(X,y)
+#print(loaded_model.predict(val_X))
+#print(result)
